b20cbef138104c5b5c43462834d5ffcca5d9a6c1
Gonzalo Albornoz <galbornoz@sgfintech.com.ar>
2025-03-11 13:44:37 -0300

    part3,  3.19 to 3.21

--- models/person.js
+++ models/person.js
@@ -2,7 +2,15 @@ const db = require("./db")
 
 const personSchema = db.createSchema({ 
     name: { type : String, minLength : 3, required : true }, 
-    number: { type : String, required : true }, 
+    number: { 
+        type : String,
+        required : true, 
+        //match : [/^([0-9]{2,3})-([0-9]{1,})$/, "invalid phone number format"],
+        validate : { 
+            validator : (value) => /^([0-9]{2,3})-([0-9]{1,})$/.test(value),
+            message: "invalid phone number format"
+        }
+    }, 
 })
 
 module.exports = db.createModel('Person', personSchema)
